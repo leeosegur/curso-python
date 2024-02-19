e57286e179b83f9833183c08673c04861c1457ea
Leeosegur <leeosegur@gmail.com>
2024-02-19 17:31:20 -0300

    update

--- prefixcalc.py
+++ prefixcalc.py
@@ -39,4 +39,40 @@ if not arguments:
 elif len(arguments) != 3:
     print(f"Numero de Argumentos invalidos, os argumentos declarados foram {arguments}")
     print("ex: `sum 5 5")
-    sys.exit("Error:1")
+    sys.exit(1)
+
+operation, *nums = arguments
+
+valid_operations = ("sum", "sub", "mul", "div")
+
+if operation not in valid_operations:
+    print("Operação invalida")
+    print(valid_operations)
+    sys.exit(1)
+
+valid_numbers = []
+
+for num in nums:
+    if not num.replace(".", " ").isdigit():
+        print(f"Numero invalido {num}")
+        sys.exit(1)
+    if "." in num:
+        num = float(num)
+    else:
+        num = int(num)
+        valid_numbers.append(num)
+
+n1, n2 = valid_numbers
+
+
+# TODO : Usar dict de funcoes
+if operation == "sum":
+    result = n1 + n2
+elif operation == "sub":
+    result = n1 - n2
+elif operation == "mul":
+    result = n1 * n2
+elif operation == "div":
+    result = n1 / n2
+
+print(f"O resultado é {result}")
