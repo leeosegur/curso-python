39ade2edb6d9cfa7ac2cdb030fcc7a17170c6a91
Leeosegur <leeosegur@gmail.com>
2024-02-20 18:48:03 -0300

    att

--- tratamento.py
+++ tratamento.py
@@ -1,28 +1,50 @@
 #!python3.12.1
-#
+
 """
 Objetivo deste programa é gerar um relatório de crianças por atividade.
 
 Imprimir a lista de crianças agrupadas por sala que frequentam cada uma das atividades.
 
 """
-#
+
 __version__ = "0.0.1"
 import os
 import sys
 
 #LBYL - LOOK BEFORE YOU LEAP
 
-if os.path.exists("names.txt"):
-    print("The file exist:")
-    input("...")    # RACE CONDITION
+#if os.path.exists("names.txt"):
+#    print("The file exist:")
+#    input("...")    # RACE CONDITION
+#    names = open("names.txt").readlines()
+#else:
+#    print("[ERROR:]File names.txt not found.")
+#    sys.exit(1)
+
+#if len(names) >= 1:
+#    print(names[1])
+#else:
+#    print("[ERROR:]Missing name in the list")
+#    sys.exit(1)
+
+###
+# EAFP - EASY TO ASK FORGIVENESS THAN PERMISSION
+###
+
+try:
     names = open("names.txt").readlines()
-else:
+except FileNotFoundError: # Bare except
     print("[ERROR:]File names.txt not found.")
     sys.exit(1)
+except AttributeError:
+    print("[ERROR:]AttributeError in file.")
+    sys.exit(1)
+#except (FileNotFoundError, AttributeError) as e:
+#   print(f"{str(e)}.")
+#   sys.exit(1)
 
-if len(names) >= 1:
+try:
     print(names[1])
-else:
+except:
     print("[ERROR:]Missing name in the list")
     sys.exit(1)
